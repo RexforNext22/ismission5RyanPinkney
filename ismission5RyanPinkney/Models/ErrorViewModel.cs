5d1f88207b6c3b7e3a8c2089f5d02e07ab9cd105
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2022-01-27 09:43:45 -0700

    added by ryan

---
+++ ismission5RyanPinkney/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ismission5RyanPinkney.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
